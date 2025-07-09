a3ba318bac4a18bed8d75a9ec326abb0c57297d2
Michael Wang <michael.yi.wang@outlook.com>
2025-07-09 10:30:33 -0400

    draft for management group deployment

--- alz-mg-deployment/main.tf
+++ alz-mg-deployment/main.tf
@@ -1,30 +1,12 @@
 terraform {
   required_providers {
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 2.2"
-    }
     alz = {
       source  = "Azure/alz"
       version = "~> 0.17.4"
     }
-    modtm = {
-      source  = "Azure/modtm"
-      version = "~> 0.3"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.9"
-    }
   }
 }
 
-# Include the additional policies and override archetypes
 provider "alz" {
   library_overwrite_enabled = true
   library_references = [
@@ -38,9 +20,58 @@ provider "alz" {
   ]
 }
 
-provider "azapi" {}
+# Deploy the recommended ALZ management group hierarchy
+module "alz_management_groups" {
+  source  = "Azure/alz/azurerm//modules/management_groups"
+  version = "~> 0.17.4"
 
+  root_management_group_id = "alz-root"
+  root_management_group_display_name = "ALZ Root"
 
-# This allows us to get the tenant id
-data "azapi_client_config" "current" {}
+  platform_management_group_id = "platform"
+  platform_management_group_display_name = "Platform"
+
+  landing_zones_management_group_id = "landing-zones"
+  landing_zones_management_group_display_name = "Landing Zones"
+
+  identity_management_group_id = "identity"
+  identity_management_group_display_name = "Identity"
+
+  connectivity_management_group_id = "connectivity"
+  connectivity_management_group_display_name = "Connectivity"
+
+  management_management_group_id = "management"
+  management_management_group_display_name = "Management"
+
+  corp_management_group_id = "corp"
+  corp_management_group_display_name = "Corp"
+
+  online_management_group_id = "online"
+  online_management_group_display_name = "Online"
+}
+
+output "root_management_group_id" {
+  value = module.alz_management_groups.root_management_group_id
+}
+output "platform_management_group_id" {
+  value = module.alz_management_groups.platform_management_group_id
+}
+output "landing_zones_management_group_id" {
+  value = module.alz_management_groups.landing_zones_management_group_id
+}
+output "identity_management_group_id" {
+  value = module.alz_management_groups.identity_management_group_id
+}
+output "connectivity_management_group_id" {
+  value = module.alz_management_groups.connectivity_management_group_id
+}
+output "management_management_group_id" {
+  value = module.alz_management_groups.management_management_group_id
+}
+output "corp_management_group_id" {
+  value = module.alz_management_groups.corp_management_group_id
+}
+output "online_management_group_id" {
+  value = module.alz_management_groups.online_management_group_id
+}
 
